3a57f7045892e3a1ea88cc32b5ef6119d3ec9503
Nevyn Bengtsson <hello@nevyn.dev>
2023-07-16 23:14:46 +0200

    Nice grid view of props

--- ShinerCoreRemote.swiftpm/CoreControlsView.swift
+++ ShinerCoreRemote.swiftpm/CoreControlsView.swift
@@ -4,11 +4,60 @@ import CoreBluetooth
 struct CoreControlsView: View {
     @ObservedObject var core: ShinerCore
     
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    var body: some View {
+        LazyVGrid(columns: columns, spacing: 16) {
+            StringPropBox(title: "Primary color", value: core.color)
+            StringPropBox(title: "Secondary color", value: core.color2)
+            StringPropBox(title: "Brightness", value: core.brightness)
+            StringPropBox(title: "Mode", value: core.mode)
+            StringPropBox(title: "Tau", value: core.tau)
+            StringPropBox(title: "Phi", value: core.phi)
+            StringPropBox(title: "Owner's name", value: core.name)
+        }
+        .navigationBarTitle(core.localName)
+    }
+}
+
+struct StringPropBox: View {
+    let title: String
+    let value: String?
+    
     var body: some View {
         VStack {
-            Text(core.color ?? "Reading...")
-                .font(.title)
-                .navigationBarTitle(core.localName ?? "Unknown")
+            Text(title)
+                .font(.headline)
+            Text(value ?? "...")
+                .font(.subheadline)
+                .foregroundColor(.gray)
         }
+        .frame(minWidth: 100, maxWidth: .infinity, minHeight: 128)
+        .padding()
+        .background(Color.gray.opacity(0.2))
+        .cornerRadius(8)
     }
-  }
+}
+
+struct SliderPropBox: View {
+    let title: String
+    let value: String?
+    let range = 0.0...255.0
+    
+    var body: some View {
+        VStack {
+            Text(title)
+                .font(.headline)
+//            Slider() // TODO...
+            Text(value ?? "...")
+                .font(.subheadline)
+                .foregroundColor(.gray)
+        }
+        .frame(minWidth: 100, maxWidth: .infinity, minHeight: 128)
+        .padding()
+        .background(Color.gray.opacity(0.2))
+        .cornerRadius(8)
+    }
+}
