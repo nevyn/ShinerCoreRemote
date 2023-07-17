f7f5edb540db1931d464d186b6668a5eab6a56f6
Nevyn Bengtsson <hello@nevyn.dev>
2023-07-17 21:06:11 +0200

    I'm deep in the rabbit hole now...
    I haven't done proper Swift in a long time

--- ShinerCoreRemote.swiftpm/CoreManager.swift
+++ ShinerCoreRemote.swiftpm/CoreManager.swift
@@ -1,25 +1,36 @@
 import CoreBluetooth
+import SwiftUI
 
 let shinerServiceUUID = CBUUID(string: "6c0de004-629d-4717-bed5-847fddfbdc2e")
-let characteristicUUIDs = [
-    "color":      CBUUID(string: "c116fce1-9a8a-4084-80a3-b83be2fbd108"),
-    "color2":     CBUUID(string: "83595a76-1b17-4158-bcee-e702c3165caf"),
-    "mode":       CBUUID(string: "70d4cabe-82cc-470a-a572-95c23f1316ff"),
-    "brightness": CBUUID(string: "2B01"),
-    "tau":        CBUUID(string: "d879c81a-09f0-4a24-a66c-cebf358bb97a"),
-    "phi":        CBUUID(string: "df6f0905-09bd-4bf6-b6f5-45b5a4d20d52"),
-    "name":       CBUUID(string: "7ad50f2a-01b5-4522-9792-d3fd4af5942f")
-]
+
+struct CoreProperty<T>
+{
+    let name: String
+    let uuid: CBUUID
+    var rawValue: String? = nil
+    let converter: PropertyConverter<T>
+    
+    func convertedValue() -> T?
+    {
+        return converter.convert(rawValue)
+    }
+    func unconvertedValue(value: T) -> String?
+    {
+        return converter.unconvert(value)
+    }
+}
 
 class ShinerCore : NSObject, Identifiable, CBPeripheralDelegate, ObservableObject
 {
-    @objc @Published var color: String?
-    @objc @Published var color2: String?
-    @objc @Published var mode: String?
-    @objc @Published var brightness: String?
-    @objc @Published var tau: String?
-    @objc @Published var phi: String?
-    @objc @Published var name: String?
+    var properties = [
+        "color": CoreProperty.create(name: "color", uuid: CBUUID(string: "c116fce1-9a8a-4084-80a3-b83be2fbd108"), converter: PropertyConverter.color()),
+        "color2": CoreProperty.create(name: "color2", uuid: CBUUID(string: "83595a76-1b17-4158-bcee-e702c3165caf"), converter: PropertyConverter.color()),
+        "mode": CoreProperty.create(name: "mode", uuid: CBUUID(string: "70d4cabe-82cc-470a-a572-95c23f1316ff"), converter: PropertyConverter.int()),
+        "brightness": CoreProperty(name: "brightness", uuid: CBUUID(string: "2B01"), converter: PropertyConverter.color()),
+        "tau": CoreProperty.create(name: "tau", uuid: CBUUID(string: "d879c81a-09f0-4a24-a66c-cebf358bb97a"), converter: PropertyConverter.number()),
+        "phi": CoreProperty.create(name: "phi", uuid: CBUUID(string: "df6f0905-09bd-4bf6-b6f5-45b5a4d20d52"), converter: PropertyConverter.number()),
+        "name": CoreProperty.create(name: "name", uuid: CBUUID(string: "7ad50f2a-01b5-4522-9792-d3fd4af5942f"), converter: PropertyConverter.string())
+    ]
         
     let device: CBPeripheral
     public init(for device: CBPeripheral)
@@ -85,14 +96,15 @@ class ShinerCore : NSObject, Identifiable, CBPeripheralDelegate, ObservableObjec
         }
         
         let uuid = characteristic.uuid
-        guard let name = characteristicUUIDs.first(where: {$0.value == uuid})?.key
+        guard let prop = properties.values.first(where: {$0.uuid == uuid})
         else { return }
         
         
-        print("Read characteristic \(name) as \(str).")
+        print("Read characteristic \(prop.name) as \(str).")
 
-        DispatchQueue.main.async { [weak self] in         
-            self?.setValue(str, forKey: name)
+        DispatchQueue.main.async { [weak self] in
+            prop.rawValue = str
+            objectWillChange.send()
         }
     }
 }
@@ -119,7 +131,7 @@ class CoreManager: NSObject, ObservableObject, CBCentralManagerDelegate
     
     func connect(to core: ShinerCore)
     {
-        print("Connecting to core \(core.name)")
+        print("Connecting to core \(core.localName)")
         centralManager.connect(core.device)
     }
     
@@ -149,7 +161,7 @@ class CoreManager: NSObject, ObservableObject, CBCentralManagerDelegate
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral)
     {
         let core = cores.first(where: {$0.device == peripheral})!
-        print("Connected to core \(core.name)")
+        print("Connected to core \(core.localName)")
         connectedCore(core)
         core.read()
     }
@@ -159,4 +171,87 @@ class CoreManager: NSObject, ObservableObject, CBCentralManagerDelegate
         // TODO: propagate to UI
         print("Failed to connect peripheral: \(error)")
     }
-  }
+}
+
+struct PropertyConverter<T>
+{
+    let convert: (String?) -> T?
+    let unconvert: (T) -> String?
+    
+    static func string() -> PropertyConverter<String> {
+        PropertyConverter<String>(
+            convert: { $0 },
+            unconvert: { $0 }
+        )
+    }
+    
+    static func int() -> PropertyConverter<Int> {
+        PropertyConverter<Int>(
+            convert: { Int($0 ?? "") },
+            unconvert: { String($0) }
+        )
+    }
+    
+    static func number() -> PropertyConverter<Double> {
+        PropertyConverter<Double>(
+            convert: { Double($0 ?? "") },
+            unconvert: { String($0) }
+        )
+    }
+    
+    static func color() -> PropertyConverter<Color> {
+        PropertyConverter<Color>(
+            convert: { rawValue in
+                guard let rawValue = rawValue else {
+                    return nil
+                }
+                
+                let components = rawValue.components(separatedBy: " ")
+                guard components.count == 3,
+                      let red = Double(components[0]),
+                      let green = Double(components[1]),
+                      let blue = Double(components[2]),
+                      red >= 0, red <= 255,
+                      green >= 0, green <= 255,
+                      blue >= 0, blue <= 255 else {
+                    return nil
+                }
+                
+                let color = Color(red: red / 255, green: green / 255, blue: blue / 255)
+                return color
+            },
+            unconvert: { color in
+                let (red, green, blue, _) = color.rgba
+                return "\(Int(red*255)) \(Int(green*255)) \(Int(blue*255))"
+            }
+        )
+    }
+}
+
+#if canImport(UIKit)
+import UIKit
+#elseif canImport(AppKit)
+import AppKit
+#endif
+
+extension Color {
+    #if canImport(UIKit)
+    var asNative: UIColor { UIColor(self) }
+    #elseif canImport(AppKit)
+    var asNative: NSColor { NSColor(self) }
+    #endif
+
+    var rgba: (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
+        let color = asNative //.usingColorSpace(.deviceRGB)!
+        var t = (CGFloat(), CGFloat(), CGFloat(), CGFloat())
+        color.getRed(&t.0, green: &t.1, blue: &t.2, alpha: &t.3)
+        return t
+    }
+
+    var hsva: (hue: CGFloat, saturation: CGFloat, value: CGFloat, alpha: CGFloat) {
+        let color = asNative //.usingColorSpace(.deviceRGB)!
+        var t = (CGFloat(), CGFloat(), CGFloat(), CGFloat())
+        color.getHue(&t.0, saturation: &t.1, brightness: &t.2, alpha: &t.3)
+        return t
+    }
+}
