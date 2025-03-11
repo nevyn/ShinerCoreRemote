966ea347d974eab1f3d991451488d846a10756e5
Nevyn Bengtsson <hello@nevyn.dev>
2025-03-11 16:49:58 +0100

    Uuuh disable broken code for now

--- ShinerCoreRemote.swiftpm/CoreControlsView.swift
+++ ShinerCoreRemote.swiftpm/CoreControlsView.swift
@@ -15,7 +15,7 @@ struct CoreControlsView: View {
                 StringPropBox(title: "Owner's name", core: core, prop: core.name)
             }
 
-            Picker("Layer",
+            /*Picker("Layer",
                 selection: Binding(get: {
                     core.layer.convertedValue() ?? 0
                 }, set: { newValue in
@@ -27,12 +27,12 @@ struct CoreControlsView: View {
                         Text($0.description).tag($0)
                     }
                 }.pickerStyle(SegmentedPickerStyle()
-            )
+            )*/
 
             LazyVGrid(columns: columns, spacing: 16) {
                 ColorPropBox(title: "Primary color", core: core, prop: core.color)
                 ColorPropBox(title: "Secondary color", core: core, prop: core.color2)
-                IntSliderPropBox(title: "Animation", core: core, prop: core.animation, range: 0.0 ... 10.0)
+                /*IntSliderPropBox(title: "Animation", core: core, prop: core.animation, range: 0.0 ... 10.0)*/
                 DoubleLogSliderPropBox(title: "Speed", core: core, prop: core.speed, range: 0.01 ... 60.0)
                 DoubleLogSliderPropBox(title: "Tau", core: core, prop: core.tau, range: 0.01 ... 80.0)
                 DoubleLogSliderPropBox(title: "Phi", core: core, prop: core.phi, range: 0.01 ... 80.0)
